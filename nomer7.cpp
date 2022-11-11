65908bc47d145dc26fbec3be609e989ccfb707a3
LukasKrisna <krisnaprasriyan@gmail.com>
2022-11-11 13:45:22 +0700

    soal nomer 7

---
+++ nomer7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+struct siswa
+{
+    string nama;
+    int nilai;
+};
+
+int main()
+{
+    siswa siswaArray[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Nama Siswa: ";
+        cin >> siswaArray[i].nama;
+        cout << "nilai: ";
+        cin >> siswaArray[i].nilai;
+    }
+
+    cout << "Siswa yang gagal: ";
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (siswaArray[i].nilai < 60)
+        {
+            cout << siswaArray[i].nama << endl;
+        }
+    }
+
+    return 0;
+}
